754df95b89b32800f71137351c8b9bb227a1b684
sgukz <thorapee055@gmail.com>
2019-10-04 13:54:27 +0700

    update

--- models/_form.php
+++ models/_form.php
@@ -1,13 +1,13 @@
 <?php
-$mysql = mysql_connect('61.19.254.3', 'rehit', 'IT@101');
-//$mysql = mysql_connect('127.0.0.1', 'root', '');
-if (!$mysql) {
-    die('Could not connect: ' . mysql_error());
-    exit;
-} else {
-    mysql_select_db("db_itreh");
-    mysql_query("SET NAMES UTF8");
-}
+// $mysql = mysql_connect('61.19.254.3', 'rehit', 'IT@101');
+// //$mysql = mysql_connect('127.0.0.1', 'root', '');
+// if (!$mysql) {
+//     die('Could not connect: ' . mysql_error());
+//     exit;
+// } else {
+//     mysql_select_db("db_itreh");
+//     mysql_query("SET NAMES UTF8");
+// }
 ?>
 <div class="modal-dialog">
     <!-- Modal content-->
@@ -26,12 +26,12 @@ if (!$mysql) {
                             <select class="form-control" name="name_comp" id="name_comp" required>
                                 <option value="" disabled>เลือกรายชื่อ</option>
                                 <?php
-                                $sql = mysql_query("SELECT * FROM tb_name_comcenter ORDER BY id_name");
-                                while ($rs = mysql_fetch_array($sql)) {
+                                // $sql = mysql_query("SELECT * FROM tb_name_comcenter ORDER BY id_name");
+                                // while ($rs = mysql_fetch_array($sql)) {
                                     ?>
                                     <option value="<?= $rs['id_name'] ?>"><?php echo $rs['name_comcenter'] ?></option>
                                 <?php
-                                }
+                                //}
                                 ?>
                             </select>
                         </div>
